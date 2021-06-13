37f26beb42c92e61f9e6f621c11b89bde6e0b062
Мегленовски Виктор <viktor.meglenovski@students.finki.ukim.mk>
2021-06-13 19:05:01 +0200

    Add project files.

---
+++ 3D Art Portfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3D_Art_Portfolio.Startup))]
+namespace _3D_Art_Portfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
